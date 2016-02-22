e0f75f68f848b54d730f60b37e949d01ea7e368c
Lacty <akira206@gmail.com>
2016-02-22 21:58:26 +0900

    impl souldUpdate: execute update once in specified balue

--- Snake/Assets/Scripts/SnakeStage.cs
+++ Snake/Assets/Scripts/SnakeStage.cs
@@ -16,6 +16,32 @@ public class SnakeStage : MonoBehaviour {
     }
   }
 
+  [SerializeField]
+  private GameObject _snakeHead = null;
+  public GameObject SnakeHead {
+    get
+    {
+      if (_snakeHead != null) return _snakeHead;
+      _snakeHead = Resources.Load<GameObject>("SnakeHead");
+      return _snakeHead;
+    }
+  }
+
+  [SerializeField]
+  private GameObject _snakeBody = null;
+  public GameObject SnakeBody {
+    get
+    {
+      if (_snakeBody != null) return _snakeBody;
+      _snakeBody = Resources.Load<GameObject>("SnakeBody");
+      return _snakeBody;
+    }
+  }
+
+  [SerializeField]
+  private const float UpdateRate = 0.6f;
+  private float _updateCount = 0.0f;
+
   private const int Columns = 10;
   private const int Rows = 10;
 
@@ -28,7 +54,16 @@ public class SnakeStage : MonoBehaviour {
 
   void Update()
   {
-    Debug.Log(Time.deltaTime);
+    if (!souldUpdate()) return;
+    Debug.Log("update");
+  }
+
+  private bool souldUpdate()
+  {
+    _updateCount += Time.deltaTime;
+    if (_updateCount <= UpdateRate) return false;
+    _updateCount -= UpdateRate;
+    return true;
   }
 
   private void InitGame()
